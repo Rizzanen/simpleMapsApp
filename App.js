a01d6a0d50391b78be6182b7c2b0c380b6ce155f
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-01-25 19:39:54 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Alert,
+  Keyboard,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  const [address, setAddress] = useState("");
+  const [coordinates, setCoordinates] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221,
+  });
+
+  const handleInputChange = (text) => {
+    setAddress(text);
+  };
+
+  const search = () => {
+    fetch(
+      `https://geocode.maps.co/search?q=${address}&api_key=65b297a62aa8d697670042rwmc299a3`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCoordinates({
+          latitude: parseFloat(data[0].lat),
+          longitude: parseFloat(data[0].lon),
+        });
+      })
+      .catch((error) => Alert.alert("Error" + error));
+    setAddress("");
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ width: "100%", height: "100%" }}>
+      <MapView
+        style={{ flex: 1, height: "80%", width: "100%" }}
+        region={{
+          latitude: coordinates.latitude,
+          longitude: coordinates.longitude,
+          latitudeDelta: coordinates.latitudeDelta,
+          longitudeDelta: coordinates.longitudeDelta,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+          }}
+          title={address}
+        />
+      </MapView>
+      <View
+        style={{
+          position: "absolute",
+          top: 50,
+          left: "8%",
+          width: "100%",
+          flexDirection: "row",
+        }}
+      >
+        <TextInput
+          style={{
+            width: "65%",
+            height: 40,
+            borderWidth: 2,
+            borderColor: "black",
+            backgroundColor: "white",
+            opacity: 0.8,
+            fontSize: 18,
+          }}
+          value={address}
+          onChangeText={(text) => handleInputChange(text)}
+        />
+        <Pressable
+          style={{
+            backgroundColor: "grey",
+            width: "18%",
+            marginLeft: 2,
+            alignItems: "center",
+            justifyContent: "center",
+            borderWidth: 2,
+            borderColor: "black",
+            padding: 5,
+            opacity: 0.9,
+          }}
+          onPress={search}
+        >
+          <Text style={{ color: "white", fontSize: 15, fontWeight: 600 }}>
+            Search
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +106,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
